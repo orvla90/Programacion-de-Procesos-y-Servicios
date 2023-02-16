8ec7d6a3353d352bc216fe8b721d4c6b1a52c2c2
KaIr0sG <114661969+KaIr0sG@users.noreply.github.com>
2023-02-16 17:23:45 +0100

    Crear Base de Datos_ Buena

---
+++ CrearBBDD/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnCrearBBDD = New System.Windows.Forms.Button()
+        Me.BtnCrearTabla = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CmbTablas = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'BtnCrearBBDD
+        '
+        Me.BtnCrearBBDD.Location = New System.Drawing.Point(30, 57)
+        Me.BtnCrearBBDD.Name = "BtnCrearBBDD"
+        Me.BtnCrearBBDD.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCrearBBDD.TabIndex = 0
+        Me.BtnCrearBBDD.Text = "Crear BBDD"
+        Me.BtnCrearBBDD.UseVisualStyleBackColor = True
+        '
+        'BtnCrearTabla
+        '
+        Me.BtnCrearTabla.Location = New System.Drawing.Point(139, 57)
+        Me.BtnCrearTabla.Name = "BtnCrearTabla"
+        Me.BtnCrearTabla.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCrearTabla.TabIndex = 1
+        Me.BtnCrearTabla.Text = "Crear Tabla"
+        Me.BtnCrearTabla.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(30, 131)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CmbTablas
+        '
+        Me.CmbTablas.FormattingEnabled = True
+        Me.CmbTablas.Location = New System.Drawing.Point(126, 131)
+        Me.CmbTablas.Name = "CmbTablas"
+        Me.CmbTablas.Size = New System.Drawing.Size(121, 21)
+        Me.CmbTablas.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.CmbTablas)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.BtnCrearTabla)
+        Me.Controls.Add(Me.BtnCrearBBDD)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents BtnCrearBBDD As System.Windows.Forms.Button
+    Friend WithEvents BtnCrearTabla As System.Windows.Forms.Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CmbTablas As ComboBox
+End Class
---
+++ CrearBBDD/Form1.vb
@@ -0,0 +1,75 @@
+﻿
+Imports System.Data.OleDb
+
+Public Class Form1
+
+    Dim cat As New ADOX.Catalog
+    Dim Cn As ADODB.Connection
+    Private Sub BtnCrearBBDD_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCrearBBDD.Click
+
+
+        Try
+            ' Generamos una nueva base de datos Access 2000, 2002 ó 2003
+            ' cat.Create("Provider = Microsoft.Jet.OLEDB.4.0;" & _
+            '            "Data Source = C:\Mis documentos\Nuevo1.mdb")
+
+            ' Generamos una nueva base de datos Access 2007-2013
+            cat.Create("Provider=Microsoft.Jet.OLEDB.4.0;" &
+                       "Data Source=C:\\BBDD_Vb\Nuevo.accdb")
+
+            ' Cerramos el objeto ADODB.Connection que ímplicitamente
+            ' se ha originado al crear el archivo de información.
+            CType(cat.ActiveConnection, ADODB.Connection).Close()
+
+            MessageBox.Show("Se ha creado con éxito la base de datos.", _
+                            "Crear base de datos", _
+                            MessageBoxButtons.OK, _
+                            MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, _
+                            "Crear base de datos", _
+                            MessageBoxButtons.OK, _
+                            MessageBoxIcon.Error)
+        Finally
+            ' Quitamos las referencias a las instancias de los objetos creados.
+            '  cat.ActiveConnection = Nothing
+            '  cat = Nothing
+
+            ' Desactivamos la opcion de crear una base de datos
+        End Try
+        BtnCrearBBDD.Enabled = False
+
+    End Sub
+
+    Private Sub BtnCrearTabla_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCrearTabla.Click
+        'Se crea conexion y catalogo(la verdad no estoy seguro de que sirve) y la tabla nueva
+
+        Dim tabla1 As ADOX.Table
+
+        Cn = New ADODB.Connection
+        '  cat = New ADOX.Catalog
+        tabla1 = New ADOX.Table
+
+        'Se abre la conexion
+        Cn.Open("Provider=Microsoft.Jet.OLEDB.4.0; Data Source=C:\vbnet\BBDD\Nuevo.accdb")
+        cat.ActiveConnection = Cn
+        tabla1.Name = "Datos"
+
+
+        'Se agregan columnas con sus nombres y tipos de datos
+        tabla1.Columns.Append("Nmat", ADOX.DataTypeEnum.adInteger)
+        'tabla1.Columns.Append("NomApel", ADOX.DataTypeEnum.adChar)
+        tabla1.Columns.Append("Nom", ADOX.DataTypeEnum.adWChar)
+        tabla1.Columns.Append("Fecha", ADOX.DataTypeEnum.adDate)
+
+        'Se agrega la tabla a la base de datos
+        cat.Tables.Append(tabla1)
+
+        'Se limpian los objetos
+        tabla1 = Nothing
+        cat = Nothing
+        Cn.Close()
+        Cn = Nothing
+    End Sub
+End Class
